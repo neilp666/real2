05f8b12acff4030039261843e1d0a04f2866a34f
neil patel <neil@myindianweddingapp.com>
2015-08-28 19:33:52 +0100

    Added associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828180148) do
+ActiveRecord::Schema.define(version: 20150828182927) do
 
   create_table "profiles", force: true do |t|
     t.string   "job_title"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20150828180148) do
     t.string   "qualifications"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
